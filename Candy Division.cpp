57598935aaef9a3405080badf0a0586c9dc87a6e
Md. Juniadul Islam <anikmym500@gmail.com>
2023-09-05 00:00:50 +0600

    first commit

---
+++ Candy Division.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int n;
+	    cin>>n;
+	    if(n%3==0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
---
+++ Masterchef finals.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int x;
+	    cin>>x;
+	    if(x<=10){
+	        cout<<"YES"<<endl;
+	    }
+	    
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
