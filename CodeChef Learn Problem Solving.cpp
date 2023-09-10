ca4fc2c92c4d3092e5f1bc81189b089666c1999b
Md. Juniadul Islam <anikmym500@gmail.com>
2023-09-11 00:25:12 +0600

    daily commit

---
+++ CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int courses;
+	cin>>courses;
+	cout<<courses*2<<endl;
+	return 0;
+}
---
+++ Lucky Seven.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin>>s;
+	char luckyletter = s[6];
+	cout<<luckyletter<<endl;
+	return 0;
+}
