02df9c74de2feb98871e6ec1908f2d83c3ce1168
Md. Juniadul Islam <anikmym500@gmail.com>
2023-10-09 00:52:59 +0600

    Streak Commit

---
+++ Minimum Coins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n--){
+	    int a;
+	    cin>>a;
+	    int coins = a%10;
+	    cout<<coins<<endl;
+	}
+	return 0;
+}
